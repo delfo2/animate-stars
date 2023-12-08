afc75100513f0b1750e39c5b1667d72e8aed6613
delfo <carlos13bem@gmail.com>
2023-12-08 07:36:22 -0300

    finish

--- code/js/Star.js
+++ code/js/Star.js
@@ -1,50 +1,67 @@
 import { getRandomNumber } from "./utils/numberHelpers.js";
 
-function drawStar(pen, x, y, rotate) {
+function drawStar(pen, x, y, rotate, size) {
+    pen.save(); // Salva o estado do contexto do canvas
+    pen.translate(x, y); // Translada para a posição (x, y)
+    pen.rotate(rotate); // Rotaciona conforme a propriedade 'rotate'
+    pen.scale(size, size);
+
     pen.beginPath();
-    pen.moveTo(x + 10, y + 10);
-    pen.lineTo(x - 4, y + 18);
-    pen.lineTo(x - 2, y + 3);
-    pen.lineTo(x - 12, y - 4);
-    pen.lineTo(x, y - 7);
-    pen.lineTo(x + 7, y - 17);
-    pen.lineTo(x + 14, y - 7);
-    pen.lineTo(x + 26, y - 5);
-    pen.lineTo(x + 17, y + 4);
-    pen.lineTo(x + 21, y + 17);
-    pen.lineTo(x + 8, y + 10);
+    for (let i = 0; i < 5; i++) {
+        pen.lineTo(
+            Math.cos(((18 + i * 72) / 180) * Math.PI) * 26,
+            -Math.sin(((18 + i * 72) / 180) * Math.PI) * 26
+        );
+        pen.lineTo(
+            Math.cos(((54 + i * 72) / 180) * Math.PI) * 10,
+            -Math.sin(((54 + i * 72) / 180) * Math.PI) * 10
+        );
+    }
     pen.fillStyle = "yellow";
     pen.fill();
-
     pen.closePath();
+
+    pen.restore();
 }
 
 export class Star {
+    alive = true;
     x = 0;
     y = 0;
+    initialX = 0;
+    initialY = 0;
     maxX = 0;
     maxY = 0;
-    alive = true;
+    rotate = 0;
+    size = 0;
+    adult = false;
 
     constructor(maxX, maxY) {
-        this.x = getRandomNumber(maxX);
-        this.y = getRandomNumber(maxY);
         this.maxX = maxX;
         this.maxY = maxY;
+        this.x = getRandomNumber(maxX);
+        this.y = getRandomNumber(maxY);
+        this.initialX = this.x;
+        this.initialY = this.y;
+        this.rotate = Math.random() * 2 * Math.PI; // Define uma rotação aleatória inicial
     }
+
     render(pen) {
         pen.save();
-        drawStar(pen, this.x, this.y, this.rotate);
-        console.log(this.rotate);
+        drawStar(pen, this.x, this.y, this.rotate, this.size);
         pen.restore();
     }
+
     animate() {
         this.aliveChecker();
         if (this.alive) {
             this.x -= 1;
             this.y += 1;
+            this.rotate += 0.02;
+            this.sizeGrowth(0.01);
         }
     }
+
     aliveChecker() {
         if (
             this.x > this.maxX ||
@@ -52,6 +69,26 @@ export class Star {
             this.y > this.maxY + 10 ||
             this.y < -10
         ) {
+            this.alive = false;
+        }
+    }
+    sizeGrowth(growth) {
+        if (!this.adult) {
+            if (this.x > this.initialX / 2 && this.y > this.initialY / 2) {
+                this.size += growth;
+            } else {
+                this.adult = true;
+            }
+
+            if (this.size > 1) {
+                this.adult = true;
+            }
+        }
+
+        if (this.adult && this.size > 0) {
+            this.size -= growth;
+        }
+        if (this.size === 0) {
             this.alive = false;
         }
     }
--- code/main.js
+++ code/main.js
@@ -2,12 +2,21 @@ import { Star } from "./js/Star.js";
 
 const canvas = document.querySelector("#canvas");
 const pen = canvas.getContext("2d");
+const maxStars = 30;
 
 const debug = document.querySelector("#debug");
+function putDebug(text) {
+    const p = document.createElement('p');
+    p.textContent = text;
+    debug.appendChild(p);
+}
+function clearDebug() {
+    debug.innerHTML = '';
+}
 
 let myStars = [];
 function createStar() {
-    if(myStars.length < 10) {
+    if(myStars.length < maxStars) {
         const star = new Star(canvas.width, canvas.height);
         myStars.push(star);
     }
@@ -22,8 +31,12 @@ function starManage() {
 }
 
 function start() {
+    clearDebug();
+
     pen.clearRect(0, 0, canvas.width, canvas.height);
     starManage();
+
+    putDebug(myStars.length);
 
     requestAnimationFrame(start);
 }
